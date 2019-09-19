628ad3cdff59a07e0941a72e265cb383bed4f0ce
Foxnova123 <52623031+Foxnova123@users.noreply.github.com>
2019-09-19 19:42:55 +0700

    *Rename

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -18,16 +18,19 @@ namespace BankAccount
         {
 
         }
+
         public BankAccount(int id)
         {
             Id = id;
         }
+
         public BankAccount(int id, decimal balance)
         {
             Id = id;
-            ; Balance = balance;
+            Balance = balance;
         }
-        public void deposit(decimal amount)
+
+        public void Deposit(decimal amount)
         {
             if (amount < 0)
             {
@@ -35,24 +38,29 @@ namespace BankAccount
             }
             else Balance = Balance + amount;
         }
-        public void withdraw(decimal amount)
+
+        public void Withdraw(decimal amount)
         {
-            if (amount > balance)
+            if (amount > Balance)
             {
-                Console.WriteLine("We looking for alien brain and we found ! you , yes you");
+                Console.WriteLine("Can't withdraw");
             }
             else if (amount < 0)
             {
-                Console.Write("Cant with draw negative amount");
+                Console.WriteLine("Can't withdraw negative amount");
             }
+            else Balance = Balance - amount;
         }
+
         public override string ToString()
         {
-            Console.WriteLine("Id:{0}", Id);
-            Console.WriteLine("Balance:{0}", Balance);
+            Console.WriteLine("Id: {0}", Id);
+            Console.WriteLine("Balance: {0}", Balance);
             return base.ToString();
         }
+
     }
+
     class Person
     {
         string name;
@@ -64,15 +72,16 @@ namespace BankAccount
             name = n;
             age = a;
         }
+
         public Person(string n, int a, List<BankAccount> acc)
         {
             name = n;
             age = a;
             accounts = acc;
         }
-        public decimal Getbalance()
-        {
 
+        public decimal GetBalance()
+        {
             decimal sum;
             sum = accounts.Sum(x => x.Balance);
             return sum;
@@ -85,15 +94,20 @@ namespace BankAccount
         {
             accounts = new List<BankAccount>();
         }
+
         public void ShowMenu()
         {
             Console.WriteLine("=== MENU ===");
             Console.WriteLine("1. Create");
             Console.WriteLine("2. Deposit");
             Console.WriteLine("3. Withdraw");
-            Console.WriteLine("4. End");
+            Console.WriteLine("4. Print");
+            Console.WriteLine("5. End");
             Console.WriteLine("=== END ===");
+
+
         }
+
         public bool IsIdExists(int id)
         {
 
@@ -103,23 +117,26 @@ namespace BankAccount
         public void AddBankAccount(int id)
         {
             accounts.Add(new BankAccount(id));
-
         }
         static void Main(string[] args)
         {
+            BankAccount acc = new BankAccount();
+
+
             Program myProgram = new Program();
             myProgram.ShowMenu();
 
-            Console.WriteLine("Enter your command: ");
+            Console.Write("Enter your command: ");
             int chooseMenu = Convert.ToInt32(Console.ReadLine());
             int chooseId;
             int chooseAmount;
             Console.WriteLine("Command: {0}", chooseMenu);
-            while (chooseMenu != 4)
+            while (chooseMenu != 5)
             {
-                if (chooseMenu > 4 || chooseMenu < 0)
+                if (chooseMenu > 5 || chooseMenu < 0)
                 {
                     Console.WriteLine("Invalid command");
+
                 }
                 else
                 {
@@ -150,7 +167,7 @@ namespace BankAccount
                             }
                             else
                             {
-                                myProgram.accounts.Find(x => x.Id == chooseId).deposit(chooseAmount);
+                                myProgram.accounts.Find(x => x.Id == chooseId).Deposit(chooseAmount);
                                 Console.WriteLine("After deposit");
                                 myProgram.accounts.Find(x => x.Id == chooseId).ToString();
                             }
@@ -166,36 +183,51 @@ namespace BankAccount
                             }
                             else
                             {
-                                myProgram.accounts.Find(x => x.Id == chooseId).withdraw(chooseAmount);
+                                myProgram.accounts.Find(x => x.Id == chooseId).Withdraw(chooseAmount);
                                 Console.WriteLine("After withdraw");
                                 myProgram.accounts.Find(x => x.Id == chooseId).ToString();
                             }
                             break;
-                        case 4: break;
+                        case 4:
+                            Console.Write("Please enter ID: ");
+                            chooseId = Convert.ToInt32(Console.ReadLine());
+                            if (!myProgram.IsIdExists(chooseId))
+                            {
+                                Console.WriteLine("Account does not exist");
+                            }
+                            else
+                            {
+                                myProgram.accounts.Find(x => x.Id == chooseId).ToString();
+                            }
+                            break;
+                        case 5: break;
                         default:
                             break;
-                            if (chooseMenu == 4) break;
-
                     }
-
-                    Console.WriteLine("Enter your  command");
-                    chooseMenu = Convert.ToInt32(Console.ReadLine());
+                    if (chooseMenu == 5) break;
                 }
-                Console.WriteLine("END PROGRAM");
-
-                List<BankAccount> listAccounts = new List<BankAccount>();
-                listAccounts.Add(new BankAccount(1, 10));
-                listAccounts.Add(new BankAccount(2, 10));
-                listAccounts.Add(new BankAccount(3, 10));
-                listAccounts.Add(new BankAccount(4, 10));
-                listAccounts.Add(new BankAccount(5, 10));
-
-                Person myPerson = new Person("Peter", 10, listAccounts);
-                Console.WriteLine("Sum: {0}", myPerson.Getbalance());
-
-
-                Console.Read();
+                Console.Write("Enter your command: ");
+                chooseMenu = Convert.ToInt32(Console.ReadLine());
             }
+
+            Console.WriteLine("END PROGRAM");
+
+            List<BankAccount> listAccounts = new List<BankAccount>();
+            listAccounts.Add(new BankAccount(1, 10));
+            listAccounts.Add(new BankAccount(2, 10));
+            listAccounts.Add(new BankAccount(3, 10));
+            listAccounts.Add(new BankAccount(4, 10));
+            listAccounts.Add(new BankAccount(5, 10));
+
+            Person myPerson = new Person("Peter", 10, listAccounts);
+
+            Console.WriteLine("Sum: {0}", myPerson.GetBalance());
+
+
+            Console.Read();
+
+
+
         }
     }
 }
